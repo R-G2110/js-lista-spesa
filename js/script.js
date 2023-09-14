3ac72569760eac0e268906df375e3ede89d3da9b
R-G2110 <argeldelacruz86@gmail.com>
2023-09-14 14:08:26 +0200

    [Add]: comments on file script

--- js/script.js
+++ js/script.js
@@ -4,21 +4,28 @@
 3. Stampare lista[i].
 4. Aumentare il contatore.
 */
-
+const listItems = document.getElementById('list-items');
 const lista = [
 	'pane',
 	'latte',
 	'uova',
 	'birra',
 	'sale',
-	'zucchero'
+	'zucchero',
+	'acqua'
 ];
 
 //1. Inizializzazione del contatore.
 let i = 0;
 
+//2. Mettere (i < lista.length) come condizione del ciclo while.
 while (i < lista.length){
 	const items = lista[i];
+
+	//3. Stampare lista[i].
 	console.log(items);
+	listItems.innerHTML += `<li class="item">${items}</li>`;
+
+	//4. Aumentare il contatore.
 	i++;
 }
